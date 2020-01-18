1760d671fbedbb5c73762a6677d2c2d4d75bbfdb
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-01-18 01:55:11 -0500

    bar chart complete

--- bundle.js
+++ bundle.js
@@ -11,30 +11,35 @@
 
     const render = (data) => {
         const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, d => d.value)])
-            .range([0, innerHeight]);
+            .domain([0, d3.max(data, d => d.value)]).nice()
+            .range([innerHeight, margin.top]);
+            
 
         const xScale = d3.scaleBand()
             .domain(data.map(d => d.name))
-            .range([0, innerWidth]);
+            .range([0, innerWidth])
+            .padding(0.1);
 
         const g = svg.append('g')
             .attr('transform', `translate(${margin.left}, ${margin.top})`);
             
         g.append('g')
             .call(d3.axisBottom(xScale))
-            .attr('transform', `translate(0, ${innerHeight})`);
+            .attr('transform', `translate(${margin.left}, ${innerHeight})`);
+        g.append('g')
+            .call(d3.axisLeft(yScale))
+            .attr('transform', `translate(${margin.left}, 0)`);
 
         g.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
-                .style('width', xScale.bandwidth()*0.95)
+                .style('width', xScale.bandwidth())
                 //.style('width', innerWidth / data.length - innerWidth/250)
-                .style('height', d => yScale(d.value))
+                .style('height', d => innerHeight - yScale(d.value))
                 //.attr('x', (d, i) => margin.left + (innerWidth / data.length) * i  )
-                .attr('x', d => xScale(d.name))    
-                .attr('y', d => innerHeight - yScale(d.value));
+                .attr('x', d => margin.left + xScale(d.name))    
+                .attr('y', d => yScale(d.value));
     };
 
     d3.csv('data.csv')
@@ -45,6 +50,7 @@
             render(data);
         });
 
-    svg.style('border', '1px solid red');
+    d3.select('svg')
+        .style('padding', '20px');
 
 }());
