f0ff86d12c8a56968290c849a17ad87ff793dca1
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-01-18 02:45:28 -0500

    backed up bar chart

--- old/barchart.js
+++ old/barchart.js
@@ -2,15 +2,21 @@ const svg = d3.select('svg');
 
 const height = +svg.attr('height');
 const width = +svg.attr('width');
-const margin = {top: 20, right: 20, bottom: 20, left: 20};
+const margin = {top: 30, right: 80, bottom: 70, left: 40};
 const innerWidth = width - margin.left - margin.right
 const innerHeight = height - margin.top - margin.bottom
 
 const render = (data) => {
+    
+    const maxminScale = (d, max, min) => {
+        return (d - min) / (max - min)
+    }
+
+    const c10 = d3.scaleSequential(d3.interpolatePuOr);
+
     const yScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.value)]).nice()
         .range([innerHeight, margin.top])
-        
 
     const xScale = d3.scaleBand()
         .domain(data.map(d => d.name))
@@ -20,12 +26,38 @@ const render = (data) => {
     const g = svg.append('g')
         .attr('transform', `translate(${margin.left}, ${margin.top})`)
         
-    g.append('g')
+    const axisG = g.append('g')
+
+    const bottomAxisG = axisG.append('g')
         .call(d3.axisBottom(xScale))
         .attr('transform', `translate(${margin.left}, ${innerHeight})`)
-    g.append('g')
+        .attr('stroke', 'white')
+
+    bottomAxisG.selectAll('.domain, .tick line')
+        .remove()
+
+    bottomAxisG.append('text')
+        .text('Letter')
+        .attr('fill', 'white')
+        .style('font-size', '1.5em')
+        .attr('x', innerWidth / 2)
+        .attr('y', margin.bottom / 1.5)
+
+    const leftAxisG = axisG.append('g')
         .call(d3.axisLeft(yScale))
         .attr('transform', `translate(${margin.left}, 0)`)
+        .attr('stroke', 'white')
+
+    leftAxisG.selectAll('.domain, .tick line')
+        .remove()
+
+    leftAxisG.append('text')
+        .text('Relative Frequency')
+        .attr('fill', 'white')
+        .style('font-size', '1.5em')
+        .attr('x', -innerHeight / 2)
+        .attr('y', -margin.left*1.5)
+        .attr('transform', 'rotate(-90)')
 
     g.selectAll('rect')
         .data(data)
@@ -37,6 +69,15 @@ const render = (data) => {
             //.attr('x', (d, i) => margin.left + (innerWidth / data.length) * i  )
             .attr('x', d => margin.left + xScale(d.name))    
             .attr('y', d => yScale(d.value))
+            .attr('fill', d => c10(maxminScale(d.value, yScale.domain()[1], yScale.domain()[0])))
+
+    g.append('text')
+        .text('Relative Frequency of Letters in the English Language')
+        .attr('fill', 'white')
+        .attr('x', innerHeight/3)
+        .attr('y', 30)
+        .style('font-size', '1.5em')
+        .style('font-family', 'Garamond')
 }
 
 d3.csv('data.csv')
@@ -48,4 +89,5 @@ d3.csv('data.csv')
     })
 
 d3.select('svg')
+    .style('background-color', '#333')
     .style('padding', '20px')
