a5d8872f4ae8f25b18322c5a44c96d776a1be5db
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-01-17 23:14:02 -0500

    smiley face

--- bundle.js
+++ bundle.js
@@ -1,10 +1,38 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	const svg = d3.select('svg');
-	const circle = svg.append('circle');
-	circle.attr('r', svg.attr('height')/3);
-	circle.attr('cx', svg.attr('width')/2);
-	circle.attr('cy', svg.attr('height')/2);
+    const svg = d3.select('svg');
+
+    const height = +svg.attr('height');
+    const width = +svg.attr('width');
+    const eyeRadius = 10;
+
+    const g = svg.append('g')
+        .attr('transform', `translate(${height/2}, ${width/2})`);
+
+    const circle = g.append('circle')
+        .attr('r', height/3)
+        .attr('fill', 'yellow')
+        .attr('stroke', 'black');
+
+    const eyeG = g.append('g')
+        .attr('transform', `translate(0, ${-height/8})`);
+
+    const lEye = eyeG.append('circle')
+        .attr('r', eyeRadius)
+        .attr('cx', -width/8);
+
+    const rEye = eyeG.append('circle')
+        .attr('r', eyeRadius)
+        .attr('cx', width/8);
+
+    g.append('path')
+        .attr('d', d3.arc()({
+            innerRadius: 70,
+            outerRadius: 80,
+            startAngle: Math.PI/2,
+            endAngle: 3*Math.PI/2,
+        }))
+        .attr('transform', 'translate(0, 20)');
 
 }());
