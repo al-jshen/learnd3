e09f0577ae5bb1d35928d0769b5b408277a2cc61
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-01-18 01:16:26 -0500

    bottom axis

--- bundle.js
+++ bundle.js
@@ -5,21 +5,36 @@
 
     const height = +svg.attr('height');
     const width = +svg.attr('width');
+    const margin = {top: 20, right: 20, bottom: 20, left: 20};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
     const render = (data) => {
         const yScale = d3.scaleLinear()
             .domain([0, d3.max(data, d => d.value)])
-            .range([0, height]);
+            .range([0, innerHeight]);
 
-        console.log(width / data.length);
-        svg.selectAll('rect')
+        const xScale = d3.scaleBand()
+            .domain(data.map(d => d.name))
+            .range([0, innerWidth]);
+
+        const g = svg.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+            
+        g.append('g')
+            .call(d3.axisBottom(xScale))
+            .attr('transform', `translate(0, ${innerHeight})`);
+
+        g.selectAll('rect')
             .data(data)
             .enter()
             .append('rect')
-                .style('width', width / data.length - 3)
+                .style('width', xScale.bandwidth()*0.95)
+                //.style('width', innerWidth / data.length - innerWidth/250)
                 .style('height', d => yScale(d.value))
-                .attr('x', (d, i) => (width / data.length) * i  )
-                .attr('y', d => height - yScale(d.value));
+                //.attr('x', (d, i) => margin.left + (innerWidth / data.length) * i  )
+                .attr('x', d => xScale(d.name))    
+                .attr('y', d => innerHeight - yScale(d.value));
     };
 
     d3.csv('data.csv')
