a27988f85dd260fc441c2d023caeacc9fb5686a6
Jeff Shen <22137276+al-jshen@users.noreply.github.com>
2020-01-24 22:45:11 -0500

    nicer colours (with scale), better syntax

--- main.js
+++ main.js
@@ -19,39 +19,48 @@ var scores = [
   {
     "name": "Evelyn",
     "score": 37
-  }
+  },
+  {
+      'name': 'Felix',
+      'score': 77}
 ]
 
 const svg = d3.select('svg');
-
 const height = +svg.attr('height');
 const width = +svg.attr('width');
+const cols = d3.scaleSequential(d3.interpolateYlOrBr);
 
-svg.selectAll('g')
+//const xScale = d3.scale.ordinal()
+//    .domain([d3.range(data.length)]).nice()
+//
+//const yScale = d3.scale.linear()
+//    .domain([0, d3.max(data, d => d.score)]).nice()
+
+const colScale = d3.scaleSequential()
+    .domain([0, 100]).nice()
+    .interpolator(d3.interpolateRainbow)
+
+const g = svg.selectAll('g')
     .data(scores)
     .enter()
     .append('g')
         .attr('class', 'item')
-        .attr('transform', (d, i) => {
-            return `translate(50, ${i*100+20})`
-        })
+        .attr('transform', (d, i) => `translate(50, ${i*100+20})`)
 
-const cols = d3.scaleSequential(d3.interpolateYlOrBr);
-
-svg.selectAll('.item')
-    .append('rect')
-        .attr('width', (d) => d.score)
-        .attr('height', (d) => d.score)
-        .attr('fill', (d) => cols(d.score/100))
+g.append('rect')
+    .attr('width', (d) => d.score)
+    .attr('height', (d) => d.score)
+    .attr('fill', (d) => colScale(d.score))
 
 
-svg.selectAll('.item')
-    .append('text')
-        .text((d, i) => {
-            return d.name
-        })
-        .style('fill', 'black')
-        .attr('font-size', 10)
-    .attr('transform', (d, i) => `translate(100, ${d.score/2})`)
+g.append('text')
+    .text((d) => d.name)
+    .style('fill', 'black')
+    .attr('font-size', 20)
+    .attr('transform', (d) => `translate(100, ${d.score/2})`)
     
-
+// svg.selectAll('.item')
+//     .sort((a, b) => {
+//         console.log(a.score - b.score)
+//         return b.score - a.score
+//     })
